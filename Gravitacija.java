8495bcc668503c2ce2061b6a813745d80c7f6bc6
jantopolovec <jantopolovec7@gmail.com>
2021-02-27 19:05:13 +0100

    vnos podatkov

--- Gravitacija.java
+++ Gravitacija.java
@@ -6,7 +6,8 @@ public class Gravitacija{
     public static final double M = (5.972) * (Math.pow(10, 24));
     public static final double r = (6.371) * (Math.pow(10, 6));
     public static void main(String[] args) {
-        double h = 1;
+	Scanner sc = new Scanner(System.in);
+        double h = sc.nextDouble();
         double a = (C*M) / Math.pow((r+(h*1000)),  2);
         System.out.println(a);
     }
