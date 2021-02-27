1df34fdb6681a0944507ea1551c49aa2e6e89da4
jantopolovec <jantopolovec7@gmail.com>
2021-02-27 14:30:54 +0100

    Dodajanje izračunapospeška #3

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Gravitacija{
+    
+    public static final double C = (6.674) * (Math.pow(10, -11));
+    public static final double M = (5.972) * (Math.pow(10, 24));
+    public static final double r = (6.371) * (Math.pow(10, 6));
     public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
+        double h = 1;
+        double a = (C*M) / Math.pow((r+(h*1000)),  2);
     }
 }
